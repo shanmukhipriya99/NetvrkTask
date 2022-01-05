9f91b2b8382ee37be4362a716576d3e3e27d9ab3
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2022-01-04 22:18:23 -0500

    added card

--- netvrk-dapp/src/App.js
+++ netvrk-dapp/src/App.js
@@ -2,30 +2,65 @@ import React from "react";
 import {
   Navbar,
   NavbarBrand,
-
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+  Button,
+  Input,
+  Col,
 } from "reactstrap";
-import './App.css';
+import "./App.css";
 
 class App extends React.Component {
-  state ={
-    
-  }
+  state = {
+    eth: null,
+  };
 
   render() {
     return (
-      <div className="App" style={{ minHeight: '100vh'}}>
+      <div className="App" style={{ minHeight: "100vh" }}>
         <div>
-            <Navbar color="dark" dark expand="md">
-              <NavbarBrand href="/">
-                Netvrk - Assignment
-              </NavbarBrand>
-            </Navbar>
-          </div>
-        
+          <Navbar dark expand="md">
+            <NavbarBrand href="/" style={{ fontSize: "20px", fontWeight: "bolder" }}>Netvrk - Assignment</NavbarBrand>
+          </Navbar>
+        </div>
+        <div>
+          <Col xl="9">
+          <Card
+          style={{
+            backgroundImage:
+              "linear-gradient(to right, #ddd6f3 35%, #faaca8 100%)",
+            padding: "20px",
+            margin: "90px", marginLeft: "390px"
+          }}
+          inverse
+        >
+          <CardBody style={{ color: "black" }}>
+            <CardTitle tag="h5">Swap ETH to TSK</CardTitle>
+            <CardText>
+            {" "}
+              <Input
+                style={{marginTop: "30px", marginBottom: "30px"}}
+                id="eth"
+                name="eth"
+                placeholder="Enter your ETH amount"
+                type="text"
+                onChange={(event) =>
+                  this.setState({ eth: event.target.value })
+                }
+              />{" "}
+            </CardText>
+            <Button>Swap</Button>
+          </CardBody>
+        </Card>
+          </Col>
+          
+        </div>
       </div>
     );
   }
-  }
-  
+}
 
 export default App;
