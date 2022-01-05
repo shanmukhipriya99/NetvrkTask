cce7a7c2b13b4ab4810c90ba9dbf26a612753846
Shanmukhi Priya <shanmukhipriya99@gmail.com>
2022-01-04 22:33:13 -0500

    completed UI

--- netvrk-dapp/src/App.js
+++ netvrk-dapp/src/App.js
@@ -16,14 +16,57 @@ import "./App.css";
 class App extends React.Component {
   state = {
     eth: null,
+    success: null,
+    tsk: null,
+    error: null
   };
 
   render() {
+    let successMessage;
+    if (this.state.success === true) {
+      successMessage = (<Col xl="8">
+      <Card
+      style={{
+        backgroundImage:
+          "linear-gradient(to right, #ddd6f3 35%, #faaca8 100%)",
+        padding: "10px",
+        margin: "40px", marginLeft: "470px"
+      }}
+      inverse
+    >
+      <CardBody style={{ color: "black" }}>
+        <CardTitle tag="h5">Success!</CardTitle>
+        <CardText>
+          You have received {this.state.tsk} TSK!
+        </CardText>
+      </CardBody>
+    </Card>
+      </Col>);
+    } else if (this.state.success === false) {
+      successMessage = (<Col xl="8">
+      <Card
+      style={{
+        backgroundImage:
+          "linear-gradient(to right, #ddd6f3 35%, #faaca8 100%)",
+        padding: "10px",
+        margin: "40px", marginLeft: "470px"
+      }}
+      inverse
+    >
+      <CardBody style={{ color: "black" }}>
+        <CardTitle tag="h5">Error!</CardTitle>
+        <CardText>
+          Error, you have not received any TSK. Please try again!
+        </CardText>
+      </CardBody>
+    </Card>
+      </Col>);
+    }
     return (
       <div className="App" style={{ minHeight: "100vh" }}>
         <div>
           <Navbar dark expand="md">
-            <NavbarBrand href="/" style={{ fontSize: "20px", fontWeight: "bolder" }}>Netvrk - Assignment</NavbarBrand>
+            <NavbarBrand href="/" style={{ fontSize: "25px", fontWeight: "bolder", color: "black" }}>Netvrk - Assignment</NavbarBrand>
           </Navbar>
         </div>
         <div>
@@ -33,7 +76,7 @@ class App extends React.Component {
             backgroundImage:
               "linear-gradient(to right, #ddd6f3 35%, #faaca8 100%)",
             padding: "20px",
-            margin: "90px", marginLeft: "390px"
+            margin: "60px", marginLeft: "390px"
           }}
           inverse
         >
@@ -52,11 +95,13 @@ class App extends React.Component {
                 }
               />{" "}
             </CardText>
-            <Button>Swap</Button>
+            <Button style={{backgroundImage:
+              "linear-gradient(to right, #aa076b 30%, #61045f 100%)"}}
+            onClick={() => this.swapfromETH()}>Swap</Button>
           </CardBody>
         </Card>
           </Col>
-          
+          {successMessage}
         </div>
       </div>
     );
